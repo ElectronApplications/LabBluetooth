1f4060a5deb509cd63f48f2907806c479f74a068
ElectronApps <barbwirek4@gmail.com>
2025-04-18 01:04:02 +0800

    Use fragments instead of activities for different screens

--- app/src/main/java/com/example/labbluetooth/MainActivity.kt
+++ app/src/main/java/com/example/labbluetooth/MainActivity.kt
@@ -1,156 +1,62 @@
 package com.example.labbluetooth
 
-import android.Manifest
-import android.annotation.SuppressLint
-import android.bluetooth.BluetoothAdapter
-import android.bluetooth.BluetoothDevice
-import android.bluetooth.BluetoothManager
-import android.content.BroadcastReceiver
-import android.content.Context
-import android.content.Intent
-import android.content.IntentFilter
-import android.os.Build
 import android.os.Bundle
-import android.util.Log
 import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
-import android.widget.Toast
-import android.widget.Toast.LENGTH_LONG
 import androidx.activity.enableEdgeToEdge
-import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.os.bundleOf
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
-import com.example.labbluetooth.database.DaoSession
-import com.example.labbluetooth.database.Device
-import com.example.labbluetooth.database.DeviceTag
-import kotlin.random.Random
-import kotlin.random.nextInt
+import androidx.fragment.app.commit
+import androidx.fragment.app.replace
 
-val defaultTags = arrayOf("Лучший друг", "Семья", "Друг", "Незнакомец", "sugar daddy", "sugar mommy")
-
-class MainActivity : AppCompatActivity() {
-//    private lateinit var bluetoothAdapter: BluetoothAdapter
-//    private lateinit var broadcastReceiver: BroadcastReceiver
-    private lateinit var chats: Array<Device>
-    private lateinit var daoSession: DaoSession
-    private var name = "Guest"
-
-    @SuppressLint("MissingPermission")
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
-        daoSession = (application as App).daoSession
-
-        name = intent.getStringExtra("username") ?: "Guest"
-        chats = daoSession.deviceDao.loadAll().toTypedArray()
-
-        val nameEditText = findViewById<EditText>(R.id.nameEditText)
-        val resultNameTextView = findViewById<TextView>(R.id.resultNameTextView)
-        val resultsView = findViewById<RecyclerView>(R.id.resultsView)
+        val mainLayoutButton = findViewById<Button>(R.id.mainLayoutButton)
         val chatLayoutButton = findViewById<Button>(R.id.chatLayoutButton)
-        val resultsRefresh = findViewById<SwipeRefreshLayout>(R.id.resultsRefresh)
-        val statsTextView = findViewById<TextView>(R.id.statsTextView)
-        val exportStatsButton = findViewById<Button>(R.id.exportStatsButton)
 
-        val statsLoader = StatsLoader(this)
-        val stats = statsLoader.data
-        if (stats.isNotEmpty()) {
-            statsTextView.append("\nВсего ${stats.sumOf { it.totalMessages }} сообщений")
-
-            val maxTotal = stats.maxBy { it.totalMessages }
-            statsTextView.append("\nБольше всего сообщений - ${maxTotal.deviceName} (${maxTotal.totalMessages})")
-
-            val maxDeleted = stats.maxBy { it.deletedMessages }
-            statsTextView.append("\nБольше всего удалено сообщений - ${maxDeleted.deviceName} (${maxDeleted.deletedMessages})")
-        }
-
-        exportStatsButton.setOnClickListener {
-            val uri = saveImage(this@MainActivity, generateStatsImage(statsLoader))
-            Toast.makeText(this@MainActivity, "Экспортировано в $uri", LENGTH_LONG).show()
-        }
-
-        nameEditText.setText(name)
-        resultNameTextView.text = name
-
-        val layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
-        resultsView.layoutManager = layoutManager
-        resultsView.adapter = DeviceItemAdapter(chats)
-
-        resultsRefresh.setOnRefreshListener {
-            val name = Random.nextInt(0x1000000).toString(16)
-
-            val device = Device(null, name, 0)
-            daoSession.deviceDao.save(device)
-
-            defaultTags.toList().shuffled().take(Random.nextInt(defaultTags.size)).forEach { tagName ->
-                val tag = DeviceTag(null, tagName, device.id)
-                daoSession.deviceTagDao.save(tag)
+        mainLayoutButton.setOnClickListener {
+            val fragment = supportFragmentManager.findFragmentById((R.id.fragmentView))
+            if (fragment is ChatFragment) {
+                fragment.saveCurrentMessages()
+                supportFragmentManager.commit {
+                    setReorderingAllowed(true)
+                    replace<MainFragment>(
+                        R.id.fragmentView,
+                        args = bundleOf("username" to fragment.username)
+                    )
+                }
             }
-
-            MessageLoader(this, name).save()
-
-            if (Random.nextFloat() > 0.75 && chats.isNotEmpty()) {
-                val lastDevice = chats.last()
-                MessageLoader(this, lastDevice.name).delete()
-                daoSession.deviceDao.delete(lastDevice)
-            }
-            chats = daoSession.deviceDao.loadAll().toTypedArray()
-
-            resultsView.adapter = DeviceItemAdapter(chats)
-            resultsRefresh.isRefreshing = false
-        }
-
-        nameEditText.setOnKeyListener { _, _, _ ->
-            name = nameEditText.text.toString()
-            resultNameTextView.text = name
-            true
         }
 
         chatLayoutButton.setOnClickListener {
-            val chatIntent = Intent(this, ChatActivity::class.java)
-            chatIntent.putExtra("username", name)
-            finish()
-            startActivity(chatIntent)
+            val fragment = supportFragmentManager.findFragmentById((R.id.fragmentView))
+            if (fragment is MainFragment) {
+                supportFragmentManager.commit {
+                    setReorderingAllowed(true)
+                    replace<ChatFragment>(
+                        R.id.fragmentView,
+                        args = bundleOf("username" to fragment.username)
+                    )
+                }
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        val fragment = supportFragmentManager.findFragmentById((R.id.fragmentView))
+        if (fragment is ChatFragment) {
+            fragment.saveCurrentMessages()
         }
 
-//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
-//            requestPermissions(arrayOf(Manifest.permission.BLUETOOTH_SCAN, Manifest.permission.BLUETOOTH_CONNECT, Manifest.permission.ACCESS_FINE_LOCATION), 42)
-//        } else {
-//            requestPermissions(arrayOf(Manifest.permission.BLUETOOTH, Manifest.permission.BLUETOOTH_ADMIN, Manifest.permission.ACCESS_FINE_LOCATION), 42)
-//        }
-
-//        bluetoothAdapter = (getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager).adapter
-//        val success = bluetoothAdapter.startDiscovery()
-//        Log.d("DEBUG", "$success")
-//
-//        broadcastReceiver = object : BroadcastReceiver() {
-//            override fun onReceive(context: Context?, intent: Intent?) {
-//                Log.d("DEBUG", "received action: ${intent?.action}")
-//                if (intent?.action == BluetoothDevice.ACTION_FOUND) {
-//                    val device: BluetoothDevice? = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE)
-//
-//                    Log.d("DEBUG", "found result: ${device?.name}")
-//                    if (device != null) {
-//                        devices.putIfAbsent(device.name, 0)
-//                        resultsTextView.text = devices.map { "${it.key} (${it.value} сообщений)" }.fold("") { acc, line -> "${acc}\n${line}" }
-//                    }
-//                }
-//            }
-//        }
-//
-//        val filter = IntentFilter(BluetoothDevice.ACTION_FOUND)
-//        registerReceiver(broadcastReceiver, filter)
+        super.onDestroy()
     }
 }
