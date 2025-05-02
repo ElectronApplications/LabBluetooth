8f88d031ba343c8470346c93a7486c6ce0ca7f54
ElectronApps <barbwirek4@gmail.com>
2025-05-02 15:33:24 +0800

    Add Data Binding (lab11)

--- app/src/main/java/com/example/labbluetooth/MainFragment.kt
+++ app/src/main/java/com/example/labbluetooth/MainFragment.kt
@@ -1,46 +1,52 @@
 package com.example.labbluetooth
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
-import android.widget.TextView
 import android.widget.Toast
 import android.widget.Toast.LENGTH_LONG
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
+import com.example.labbluetooth.databinding.FragmentMainBinding
 
 class MainFragment : Fragment(R.layout.fragment_main) {
+    private lateinit var viewModel: MainViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        viewModel = ViewModelProvider(requireActivity())[MainViewModel::class.java]
+
+        val binding: FragmentMainBinding =
+            DataBindingUtil.inflate(inflater, R.layout.fragment_main, container, false)
+
+        binding.viewModel = viewModel
+        binding.lifecycleOwner = this
+
+        return binding.root
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val viewModel = ViewModelProvider(requireActivity())[MainViewModel::class.java]
-
         val nameEditText = view.findViewById<EditText>(R.id.nameEditText)
-        val resultNameTextView = view.findViewById<TextView>(R.id.resultNameTextView)
         val resultsView = view.findViewById<RecyclerView>(R.id.resultsView)
         val resultsRefresh = view.findViewById<SwipeRefreshLayout>(R.id.resultsRefresh)
-        val statsTextView = view.findViewById<TextView>(R.id.statsTextView)
         val exportStatsButton = view.findViewById<Button>(R.id.exportStatsButton)
 
         nameEditText.setText(viewModel.username)
 
         viewModel.uiState.observe(viewLifecycleOwner) { uiState ->
-            resultNameTextView.text = uiState.username
             resultsView.adapter = DeviceItemAdapter(uiState.chats.toTypedArray())
-
-            val stats = uiState.stats
-            if (stats.isNotEmpty()) {
-                statsTextView.text = ""
-                statsTextView.append("\nВсего ${stats.sumOf { it.totalMessages }} сообщений")
-                val maxTotal = stats.maxBy { it.totalMessages }
-                statsTextView.append("\nБольше всего сообщений - ${maxTotal.deviceName} (${maxTotal.totalMessages})")
-                val maxDeleted = stats.maxBy { it.deletedMessages }
-                statsTextView.append("\nБольше всего удалено сообщений - ${maxDeleted.deviceName} (${maxDeleted.deletedMessages})")
-            }
         }
 
         exportStatsButton.setOnClickListener {
